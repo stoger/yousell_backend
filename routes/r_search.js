959027be5bc3889e87885d0220e55746121b1591
Stephan Stöger <stephan.stoeger@gmail.com>
2018-02-05 08:10:03 +0100

    Update

--- routes/r_search.js
+++ routes/r_search.js
@@ -8,11 +8,9 @@ let searchProduct = require('../models/m_product').searchProduct;
 
 // Home page route.
 router.get('/', function (req, res) {
-
-    searchProduct(req.query.q).then(function(result) {
+    searchProduct(req.query.q).then(function (result) {
         res.status(200).send(result);
     });
-    
-  });
+});
 
-  module.exports = router;
+module.exports = router;
